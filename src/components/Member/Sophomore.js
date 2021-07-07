39cfdd4361155a47722a71278665d63029b9cc5e
shiwani jha <shiwanikumari835@gmail.com>
2021-07-07 16:50:03 +0530

    modify sophomore page

--- src/components/Member/Sophomore.js
+++ src/components/Member/Sophomore.js
@@ -5,8 +5,38 @@ export default function Sophomores() {
     return (
         <Screen>
         <div>
-            
+          <h1>Sophomores</h1> 
+          <div className='list-container'>
+            <div className='list-child'>
+              <Content
+               imageUrl=""
+               name="shiwani kumari"
+               batch="2k20, ECE"
+               link=""/>
+            </div>
+            <div className='list-child'>
+              <Content
+               imageUrl=""
+               name="shiwani kumari"
+               batch="2k20, ECE"
+               link=""/>
+            </div>
+
+        </div>
         </div>
         </Screen>
     )
 }
+
+  const Content = ({ imageUrl, name,batch,link }) => {
+    return (
+        <div className="content">
+            <img src={imageUrl} alt="profile-pic" style="width:100%"/>
+            <h4>{name}</h4>
+            <p>{batch}</p>
+            <a href={link}>View profile</a>
+        </div>
+    );
+  };
+
+
