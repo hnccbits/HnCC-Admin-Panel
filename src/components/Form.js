bed3cc9748e318d7a1137d35cbc57d1437329492
rishab0902 <rishab.davksd@gmail.com>
2021-07-11 17:22:56 +0530

    bug fixes in forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,71 +1,90 @@
-import React from 'react'
-
-
+import React from "react";
 
 export default class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        
-        <form onSubmit={this.handleSubmit}>
-           <caption>Create new meet</caption>
-             <div className="row">
-                  <div className="col-25">
-                     <label for="agenda">Agenda</label>
-                  </div>
-                  <div className="col-75">
-                     <input type="text"  value={this.state.value} onChange={this.handleChange}id="agenda"  placeholder="agenda of the meet"/>
-                  </div>
-             </div>
-            <div className="row">
-                  <div className="col-25">
-                      <label for="date">Date</label>
-                  </div>
-                  <div className="col-75">
-                    <input type="date"  value={this.state.value} onChange={this.handleChange} id="date" />
-                  </div>
-            </div>
-            <div className="row">
-                  <div className="col-25">
-                      <label for="time">Time</label>
-                  </div>
-                  <div className="col-75">
-                    <input type="time"  value={this.state.value} onChange={this.handleChange} id="time"/>
-                  </div>
-            </div>
-            <div className="row">
-                  <div className="col-25">
-                      <label for="meetlink">Link</label>
-                  </div>
-                  <div className="col-75">
-                    <input type="link"  value={this.state.value} onChange={this.handleChange} id="meetlink"/>
-                  </div>
-            </div>
-           
-           <div className="row">
-                <input className="submit" type="submit" value="Submit"/>
-            </div>
-         </form>
-  
-       
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+    this.state = { date: "" };
+    this.state = { time: "" };
+    this.state = { link: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({ text: event.target.text });
+    this.setState({ date: event.target.date });
+    this.setState({ time: event.target.time });
+    this.setState({ link: event.target.link });
+  }
 
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <caption>Create new meet</caption>
+        <div className="row">
+          <div className="col-25">
+            <label for="agenda">Agenda</label>
+          </div>
+          <div className="col-75">
+            <input
+              type="text"
+              value={this.state.text}
+              onChange={this.handleChange}
+              id="agenda"
+              placeholder="agenda of the meet"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-25">
+            <label for="date">Date</label>
+          </div>
+          <div className="col-75">
+            <input
+              type="date"
+              value={this.state.date}
+              onChange={this.handleChange}
+              id="date"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-25">
+            <label for="time">Time</label>
+          </div>
+          <div className="col-75">
+            <input
+              type="time"
+              value={this.state.time}
+              onChange={this.handleChange}
+              id="time"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-25">
+            <label for="meetlink">Link</label>
+          </div>
+          <div className="col-75">
+            <input
+              type="link"
+              value={this.state.link}
+              onChange={this.handleChange}
+              id="meetlink"
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <input className="submit" type="submit" value="Submit" />
+        </div>
+      </form>
+    );
+  }
+}
